8f490455083a4f8d19b31b760581782bb6e259e7
Sachin4507243 <pahaniasachin554@gmail.com>
2025-06-03 22:40:35 +0530

    change front cors url

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Your frontend origin
+    origin: "https://you-tube-clone-frontend-alpha.vercel.app/", // Your frontend origin
     credentials: true, // Allow credentials (cookies)
   })
 );
